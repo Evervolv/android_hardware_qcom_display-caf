f44c7cd94c18ff338f6a891824d91a6b22989f50
dianlujitao <dianlujitao@lineageos.org>
2019-09-06 11:19:55 +0800

    display: Use generated kernel headers
    Change-Id: I7cdd8c2ac14597ed0ddd458de5a6ee9ceff89380

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -14,16 +14,11 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libsync libgralloc
 ifeq ($(TARGET_KERNEL_VERSION), 4.14)
 LOCAL_C_INCLUDES              += external/libcxx/include \
                                  system/core/libion/include/ \
-                                 system/core/libion/kernel-headers/ \
-                                 $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+                                 system/core/libion/kernel-headers/
 LOCAL_SHARED_LIBRARIES        += libion
 endif
-LOCAL_HEADER_LIBRARIES        := display_headers
-ifneq ($(TARGET_KERNEL_VERSION), 4.14)
-LOCAL_CFLAGS                  += $(kernel_includes)
-endif
+LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
 LOCAL_CLANG                   := true
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := gr_ion_alloc.cpp \
                                  gr_allocator.cpp \
                                  gr_buf_mgr.cpp \
@@ -42,14 +37,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := libgrallocutils
 LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_C_INCLUDES              := $(common_includes)
 ifeq ($(TARGET_KERNEL_VERSION), 4.14)
 LOCAL_C_INCLUDES              += system/core/libion/include \
                                  system/core/libion/kernel-headers
 endif
-LOCAL_HEADER_LIBRARIES        := display_headers
+LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libdl android.hardware.graphics.common@1.1
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"grallocutils\" -Wno-sign-conversion
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp
 include $(BUILD_SHARED_LIBRARY)
